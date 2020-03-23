07ba7dc3725cc94d537f3bf05c23857c9a74be23
Ronald @ home <r.a.remmerswaal@rug.nl>
2020-03-23 10:12:10 +0100

    Using iterator

--- examples/flower.jl
+++ examples/flower.jl
@@ -7,57 +7,31 @@ error_tolerance = 1E-2
 flower(x) = 1 / (1E-1 + (x[1]^2 + x[2]^2 - 0.25 * (1 + 0.0 * cos(5 * atan(x[2], x[1]) + 1)))^2)
 
 tree = initialize_quadtree([0., 0.], flower)
-refine!(tree, flower)
 
 function adaptive_refinement!(tree, fun, max_level, error_tolerance)
+    if isleaf(tree) refine!(tree, fun) end
 
     for level = 2:max_level
         marked = Vector{Quad}()
 
-        indices = Vector{Int64}()
-        quads = Vector{Quad}()
-        push!(indices, 1)
-        push!(quads, tree)
-        depth = 0
         max_error = 0.
         nr_cells = 0
-        while true
-            if indices[end] < 5
-                quad = quads[end]
+        nr_leaf_cells = 0
+        for quad ∈ tree
+            nr_cells += 1
 
-                nr_cells += 1
-                if isleafparent(quad)
-                    error_estimate = abs(quad.state - sum([child.state for child ∈ quad.children]) / 4)
-                    max_error = max(error_estimate, max_error)
-                    if error_estimate > error_tolerance
-                        push!(marked, quad)
-                    end
-                end
-
-                if isleaf(quads[end])
-                    indices[end] += 1
-                    if indices[end] < 5
-                        quads[end] = quads[end-1].children[indices[end]]
-                    end
-                else
-                    push!(quads, quad.children[1])
-                    push!(indices, 1)
-                end
-            else
-                pop!(quads)
-                pop!(indices)
-                if length(indices) == 1
-                    break
-                end
-                indices[end] += 1
-                if indices[end] < 5
-                    quads[end] = quads[end-1].children[indices[end]]
+            if isleaf(quad) nr_leaf_cells += 1 end
+            if isleafparent(quad)
+                error_estimate = abs(quad.state - sum([child.state for child ∈ quad.children]) / 4)
+                max_error = max(error_estimate, max_error)
+                if error_estimate > error_tolerance
+                    push!(marked, quad)
                 end
             end
 
         end
 
-        print("At level $level we marked $(4*length(marked)) cells (out of $nr_cells, uniform $(2<<(2*level))) with max error = $max_error\n")
+        print("At level $level we marked $(4*length(marked)) cells (leaf/uniform = $(nr_leaf_cells/(1<<(2*(level-1))))) with max error = $max_error\n")
 
         if length(marked) == 0
             break
@@ -66,6 +40,16 @@ function adaptive_refinement!(tree, fun, max_level, error_tolerance)
         refine!(marked, fun, recurse=true)
     end
 
+end
+
+function nary(number::Int, n::Int)
+    rep = Vector{Int}()
+    while number > 0
+        rem = mod(number, n)
+        push!(rep, rem)
+        number = (number - rem) / n
+    end
+    return rep
 end
 
 adaptive_refinement!(tree, flower, max_level, error_tolerance)
--- src/interface.jl
+++ src/interface.jl
@@ -1,10 +1,32 @@
-function iterate(quad::Quad)
-    return initialized(quad) ? quad : nothing
-end
+function Base.iterate(quad::Quad, state=initialized(quad) ? (quad, 0, Vector{Int}())  : nothing)
+    element, count, indices = state
 
-function iterate(quad::Quad, state)
-    parent = state.parent
+    if element == nothing return nothing end
 
-    # How to find myself? == === is a bad idea since this will recurse indefinitely
+    if isleaf(element)
+        indices[end] += 1
+        if indices[end] < 5
+            next_element = element.parent.children[indices[end]]
+        end
+    else
+        next_element = element.children[1]
+        push!(indices, 1)
+    end
 
+    if indices[end] >= 5
+        next_element = element
+        while indices[end] >= 5
+            next_element = next_element.parent
+            pop!(indices)
+            if isempty(indices)
+                next_element = nothing
+                break
+            end
+            indices[end] += 1
+            if indices[end] < 5
+                next_element = next_element.parent.children[indices[end]]
+            end
+        end
+    end
+    return (element, (next_element, count + 1, indices))
 end
