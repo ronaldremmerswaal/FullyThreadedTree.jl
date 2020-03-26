adbe0c2e0b16648f182052a89d21fdde55e277f6
Ronald @ home <r.a.remmerswaal@rug.nl>
2020-03-26 20:15:52 +0100

    Plotting of grid nodes / faces

--- src/plotting.jl
+++ src/plotting.jl
@@ -7,16 +7,36 @@ function plot(tree::Tree{2})
     Y = Vector()
     Z = Vector()
     max_level = 0
-    for leaf ∈ active_cells(tree)
+    for cell ∈ active_cells(tree)
         append!(X, NaN)
         append!(Y, NaN)
         # append!(Z, 1.)
-        append!(X, leaf.position[1] .+ [-1., 1., 1., -1., -1.] / (2 << (leaf.level)))
-        append!(Y, leaf.position[2] .+ [-1., -1., 1., 1., -1.] / (2 << (leaf.level)))
+        append!(X, cell.position[1] .+ [-1., 1., 1., -1., -1.] / (2 << (cell.level)))
+        append!(Y, cell.position[2] .+ [-1., -1., 1., 1., -1.] / (2 << (cell.level)))
         # append!(Z, leaf.state)
-        max_level = max(max_level, leaf.level)
+        max_level = max(max_level, cell.level)
     end
     # plot(X, Y, legend=false, seriestype=:shape, fill_z=Z, linewidth=2. / (1 << max_level))
     plot(X, Y, legend=false, seriestype=:shape, linewidth=2. / (1 << max_level))#, fill_z=Z
+
+
+    X = Vector()
+    Y = Vector()
+    for cell ∈ active_cells(tree)
+        pos = centroid(cell)
+        push!(X, pos[1])
+        push!(Y, pos[2])
+    end
+
+    plot!(X, Y, seriestype=:scatter, markersize = 20. /  (1 << max_level), marker=:circle)
+
+    X = Vector()
+    Y = Vector()
+    for face ∈ active_faces(tree)
+        pos = centroid(face)
+        push!(X, pos[1])
+        push!(Y, pos[2])
+    end
+    plot!(X, Y, seriestype=:scatter, markersize = 20. /  (1 << max_level), marker=:square)
 
 end
