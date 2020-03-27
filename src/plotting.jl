2eb77fbd09bdbee2db990c19a07969d7689fc29d
Ronald @ home <r.a.remmerswaal@rug.nl>
2020-03-27 10:00:24 +0100

    Plotting options

--- src/plotting.jl
+++ src/plotting.jl
@@ -35,7 +35,7 @@ function plot(tree::Tree{2}; markers::Bool = false, max_marker_level::Int = 5, p
     end
     linewidth0 = 4.
     linewidth = linewidth0 / (1 << max_level)
-    plot(X, Y, legend = false, seriestype = :shape, fill = :white, linewidth = linewidth)
+    plot(X, Y, legend = false, color=:black, linewidth = linewidth)
 
     if path
         X = Vector()
@@ -72,7 +72,88 @@ function plot(tree::Tree{2}; markers::Bool = false, max_marker_level::Int = 5, p
                 push!(Y, pos[2])
             end
         end
-        plot!(X, Y, seriestype = :scatter, markersize = markersize, markerstrokewidth = markerstrokewidth, marker=:square)
+        plot!(X, Y, seriestype = :scatter, markersize = markersize, markerstrokewidth = markerstrokewidth, marker = :square)
+    end
+
+    return current()
+end
+
+function plot(tree::Tree{3}; markers::Bool = false, max_marker_level::Int = 5, path::Bool = false, wireframe=true)
+    X = Vector()
+    Y = Vector()
+    Z = Vector()
+
+    max_level = levels(tree) - 1
+    linewidth0 = 4.
+    linewidth = linewidth0 / (1 << max_level)
+    if wireframe
+        xloop = [-1., 1., 1., -1., -1.]
+        yloop = [-1., -1., 1., 1., -1.]
+        for cell ∈ active_cells(tree)
+            append!(X, NaN)
+            append!(Y, NaN)
+            append!(Z, NaN)
+
+            append!(X, cell.position[1] .+ xloop / (2 << (cell.level)))
+            append!(Y, cell.position[2] .+ yloop / (2 << (cell.level)))
+            append!(Z, cell.position[3] .+ -ones(5) / (2 << (cell.level)))
+            append!(X, cell.position[1] .+ xloop / (2 << (cell.level)))
+            append!(Y, cell.position[2] .+ yloop / (2 << (cell.level)))
+            append!(Z, cell.position[3] .+ ones(5) / (2 << (cell.level)))
+            for i=2:4
+                append!(X, NaN)
+                append!(Y, NaN)
+                append!(Z, NaN)
+                append!(X, cell.position[1] .+ [xloop[i], xloop[i]] / (2 << (cell.level)))
+                append!(Y, cell.position[2] .+ [yloop[i], yloop[i]] / (2 << (cell.level)))
+                append!(Z, cell.position[3] .+ [-1., 1.] / (2 << (cell.level)))
+            end
+        end
+        plot(X, Y, Z, legend = false, color = :black, linewidth = linewidth)
+    end
+
+    if path
+        X = Vector()
+        Y = Vector()
+        Z = Vector()
+        for cell ∈ active_cells(tree)
+            pos = centroid(cell)
+            push!(X, pos[1])
+            push!(Y, pos[2])
+            push!(Z, pos[3])
+        end
+        plot!(X, Y, Z, linewidth = linewidth, color = :lightgray)
+    end
+
+    if markers
+        markerstrokewidth = linewidth0 /  (1 << min(max_marker_level, max_level))
+        markersize = 20. /  (1 << min(max_marker_level, max_level))
+
+        X = Vector()
+        Y = Vector()
+        Z = Vector()
+        for cell ∈ active_cells(tree)
+            if cell.level < max_marker_level
+                pos = centroid(cell)
+                push!(X, pos[1])
+                push!(Y, pos[2])
+                push!(Z, pos[3])
+            end
+        end
+        plot!(X, Y, Z, seriestype = :scatter, markersize = markersize, markerstrokewidth = markerstrokewidth, marker = :circle)
+
+        X = Vector()
+        Y = Vector()
+        Z = Vector()
+        for face ∈ active_faces(tree)
+            if level(face) < max_marker_level
+                pos = centroid(face)
+                push!(X, pos[1])
+                push!(Y, pos[2])
+                push!(Z, pos[3])
+            end
+        end
+        plot!(X, Y, Z, seriestype = :scatter, markersize = markersize, markerstrokewidth = markerstrokewidth, marker = :square)
     end
 
     return current()
